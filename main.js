43804f9b3f13fee24033e5e1c51bd436364bb624
TanyaIndian <97530331+TanyaIndian@users.noreply.github.com>
2022-03-02 23:51:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+ let a=40;
+ let b=20;
+ const add=require("./add")
+
+ const sub=require("./sub")
+ const mul=require("./multiple")
+ const div=require("./divide")
+ console.log(add(a,b),sub(a,b),mul(a,b),div(a,b))
